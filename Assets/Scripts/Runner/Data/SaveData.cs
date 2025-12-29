1f60d07b2344c1ba9009e3a91695602ade18cf84
jgpark009 <jgpark009@gmail.com>
2025-12-29 18:35:14 +0900

    에디터 씬 작업중

---
+++ Assets/Scripts/Runner/Data/SaveData.cs
@@ -0,0 +1,27 @@
+using Assets.Scripts.FrameWork.Job;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stage",menuName = "XPUP/STAGE")]
+public class SaveData : ScriptableObject
+{
+    public int X = 4;
+
+    public int Y = 4;
+
+    public int StageLevel = 1;
+    public int StageTime = 600;
+
+    public int StageEXP = 40;
+
+    public int ItemKind = 4;
+
+    public bool LockGame = false;
+
+    public Vector2[] LockPos;
+
+    public bool BlckGame = false;
+
+    public bool DirtyGame = false;
+
+
+}
