a5435cbaa8f3f3f3f10312d8623cdd4c985d82da
jgpark009 <jgpark009@gmail.com>
2026-01-02 15:51:21 +0900

    스테이지 구성완료

--- Assets/Scripts/Runner/Popup/PopupStageStart.cs
+++ Assets/Scripts/Runner/Popup/PopupStageStart.cs
@@ -1,5 +1,5 @@
 using Assets.Scripts.FrameWork.Job;
-
+using TMPro;
 using UnityEngine.UI;
 
 public class PopupStageStart : Popup
@@ -9,6 +9,8 @@ public class PopupStageStart : Popup
 
 
     Button Button_Play;
+
+    TextMeshProUGUI Text_Title;
     private void Initialize()
     {
         Dimed = transform.Find("Dimed").GetComponent<Button>();
@@ -17,11 +19,15 @@ public class PopupStageStart : Popup
 
         Button_Play = transform.Find("Popup/Button_Play").GetComponent<Button>();
         Button_Play.onClick.AddListener(OnStagePlay);
+
+        Text_Title = transform.Find("Popup/Top/Text_Title").GetComponent<TextMeshProUGUI>();
         Dimed.onClick.AddListener(OnClose);
 
         Button_Close.onClick.AddListener(OnClose);
 
         gameObject.SetActive(true);
+
+        Text_Title.text = "Level_" +( DataManager.I.prevData.CurrentStage+1);
     }
 
     protected override void OnOpen(object args)
